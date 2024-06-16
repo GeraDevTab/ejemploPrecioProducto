4d2b97dc215cfc7679cb1b949ab99b3c134c6c7d
Gerardo <everythingissofast@gmail.com>
2024-06-16 15:40:49 -0600

    primer commit producto y cambio de precio

---
+++ src/MainProducto.java
@@ -0,0 +1,8 @@
+public class MainProducto {
+    public static void main(String[] args) {
+        Producto p1 = new Producto("Lapiz", 1.50);
+        Producto p2 = p1;
+        p2.setPrecio(2.00);
+        System.out.println(p1.getPrecio());
+    }
+}
